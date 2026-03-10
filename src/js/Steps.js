a0f209ae967fcd53295390f841259da1319552ab
Adelina1905 <temciucadelina14@gmail.com>
2026-03-10 21:19:56 +0200

    Added dynamic card in recipe

--- src/js/Steps.js
+++ src/js/Steps.js
@@ -1,27 +1,82 @@
 export class Steps {
-    constructor(id, type, sizes, recipe) {
-        this.id = id;
-        this.type = type;
-        this.sizes = sizes;
-        this.recipe = recipe;
-    }
-    renderRecipe() {
-        const el = document.createElement('div');
+  constructor(id, type, sizes, recipe) {
+    this.id = id;
+    this.type = type;
+    this.sizes = sizes;
+    this.recipe = recipe;
+    this.index = 0;
+  }
+  render() {
+    this.el = document.createElement('div');
+    this.el.className = "cocktail-recipe";
+    this.el.innerHTML = `
+      ${this.renderRecipe()}
 
-        el.className = "cocktail-recipe";
-        const ingredients = this.recipe.ingredients;
-        console.log(ingredients);
-        el.innerHTML = `
-            <div class = "title">
-                <img src = ""/>
-                <ul>
-                ${ingredients.map(data => 
-            `<li>${data}</li>`
-        ).join("")}
-                </ul>
-            </div>
-            <div class = "card-board"></div>
-        `
-        return el;
-    }
+      <div class="card-container">
+        ${this.renderCard()}
+      </div>
+      <button id="back-card-btn"><img src="../images/Arrow.png"alt="back button"></button>
+      <button id="front-card-btn"><img src="../images/Arrow.png"alt="back button"></button>
+    `;
+    this.cardContainer = this.el.querySelector(".card-container");
+
+    this.updateIndex();
+    return this.el;
+  }
+
+  renderRecipe() {
+    return `
+      <div class = "title">
+          <img src = "../images/New(${this.id}).png" alt = "Image of ${this.type}" />
+          <h1>${this.type}</h1>
+      </div>
+    `;
+  }
+
+  renderCard() {
+    const sections = [
+    this.recipe.ingredients,
+    this.recipe.preparation,
+    this.recipe.tips
+  ];
+
+  const titles = ["Ingredients", "Preparation", "Tips"];
+
+  const currentList = sections[this.index];
+  const currentTitle = titles[this.index];
+
+  return this.renderList(currentList, currentTitle);
+  }
+
+  renderList(list, title) {
+    return `
+    <div class = "card">
+      <h3 class="card-title">${title}</h3>
+      <ul>
+        ${list.map(data => `<li>${data}</li>`).join("")}    
+      </ul>
+    </div>
+    `;
+  }
+
+  updateIndex() {
+    const button_b = this.el.querySelector('#back-card-btn');
+    const button_f = this.el.querySelector('#front-card-btn');
+    button_b.addEventListener("click", () => {
+      this.index--;
+      if(this.index < 0) this.index = 2;
+      this.refreshCard();
+    })
+      button_f.addEventListener("click", () => {
+      this.index++;
+      if(this.index > 2) this.index = 0;    
+
+      this.refreshCard();
+    })
+
+
+  }
+  refreshCard() {
+    this.cardContainer.innerHTML = this.renderCard();
+  }
 }
--- src/js/recipe.js
+++ src/js/recipe.js
@@ -8,4 +8,4 @@ const container = document.getElementById("recipe-page");
 const cocktail = COCKTAILS.find(c => c.id === id);
 
 const recipe = new Steps(cocktail.id, cocktail.type, cocktail.sizes, cocktail.recipe);
-container.appendChild(recipe.renderRecipe());
+container.appendChild(recipe.render());
